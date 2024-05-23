2d60f28770121cef1831a67bee423df9f7ee40ae
SammyMusyoki <sammerkhan328@gmail.com>
2024-05-23 14:23:16 +0300

    Added functionality to send mail to mailbox

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,19 @@
-export interface Questions {
+interface Questions {
     id: number;
+    name: string;
     questionText: string;
     answer: string;
     placeholder: string;
+};
+
+interface MessageContextValue {
+    message: string;
+    type: 'success' | 'error' | 'warning' | null;
+    showMessage: (msg: string, type: "success" | "error" | "warning") => void;
+    clearMessage: () => void;
+}
+
+export type {
+    Questions,
+    MessageContextValue,
 }
