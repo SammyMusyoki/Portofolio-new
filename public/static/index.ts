49bab947ec8dd3a60b7afba60cdcccaf08ebb2a6
SammyMusyoki <sammerkhan328@gmail.com>
2024-06-14 13:07:57 +0300

    minor changes

--- public/static/index.ts
+++
@@ -1,7 +0,0 @@
-import SammyMusyoki from './images/sammy-musyoki.png'
-import SammyMusyoki2 from './images/sammy-musyoki2.png'
-
-export {
-    SammyMusyoki,
-    SammyMusyoki2,
-}
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,20 @@
-// import React from 'react'
-// import { AboutMe } from './sections/about-me'
-// import { Metadata } from "next";
+import React from 'react'
+import { AboutMe } from './sections/about-me'
+import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "About Me",
-//   description:
-//     "Information about who is Sammy Musyoki, my knowledge, statistics about me and social links ",
-//   keywords: ["about", "social", "links", "knowledge"],
-// };
+export const metadata: Metadata = {
+  title: "About Me",
+  description:
+    "Information about who is Sammy Musyoki, my knowledge, statistics about me and social links ",
+  keywords: ["about", "social", "links", "knowledge"],
+};
 
-// const Page = () => {
-//   return (
-//     <div className='m-auto space-y-14'>
-//       <AboutMe />
-//     </div>
-//   )
-// }
+const Page = () => {
+  return (
+    <div className='m-auto space-y-14'>
+      <AboutMe />
+    </div>
+  )
+}
 
-// export default Page
+export default Page
